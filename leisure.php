fd7c05fdf72c8c47b52a4827055c59f261d720d5
DevNexus Tec. <160817446+Okinda-joseph@users.noreply.github.com>
2024-09-14 13:35:09 +0300

    Create leisure.php

---
+++ leisure.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- The required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Leisure activities</title>
+  <link rel="stylesheet" href="./css/boostrap.min.css">
+  <link rel="stylesheet" href="./css/boxicons.min.css">
+  <link rel="stylesheet" href="./css/owl.carousel.min.css">
+  <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="./css/styles.css">
+  <script src="https://kit.fontawesome.com/5bebd170da.js" crossorigin="anonymous"></script>
+  <!-- favicon -->
+  <link rel="shortcut icon" href="./assets/icons/logo.jpeg" type="image/x-icon">
+</head>
+
+<body>
+  <?php
+  include("components/header.html");
+  ?>
+  <!-- GENERAL BANNER -->
+  <div class="main-banner-wrapper">
+    <div class="main-banner d-flex align-items-center bg-cover" id="leisure-banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="title-wrap">
+              <h1 class="title display-3 text-capitalize text-white">Leisure activities</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- LEISURE ACTIVITY BODY -->
+  <section id="main-content">
+    <div class="content-box-wrapper">
+      <div class="container">
+        <div class="main-title-wrap mb-4">
+          <h4 class="title">Experience the Ultimate Thrills</h4>
+        </div>
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="row">
+              <div class="col-12">
+                <div class="content-box">
+                  <div class="text-wrap main-text">
+                    <p>
+                      Indulge in a world of excitement and joy with our unbeatable range of leisure activities! At Kamel Park Hotel, we take leisure to the next level, offering an array of thrilling experiences that will create unforgettable memories for you and your loved ones.
+                    <p>
+                    <p>&nbsp;</p>
+                    <div class="main-img-box"><img loading="lazy" src="./assets/banner/pool-banner.jpg" alt=""></div>
+                    </p>
+                    <p>&nbsp;</p>
+                    <p>
+                      Zip Lining: Soar through the sky as you zip across breathtaking landscapes, feeling the rush of adrenaline and the sheer exhilaration of defying gravity. Our professionally designed zip lines guarantee an adventure like no other.
+                    </p>
+                    <p>&nbsp;</p>
+                    <p>
+                      Children's Car Rides: Let your little ones embark on a whimsical journey of their own. Our charming and safe children's car rides will transport them to a magical world filled with laughter and imagination.
+                    </p>
+                    <p>&nbsp;</p>
+                    <p>
+                      Day Care: Relax and unwind while your kids engage in a world of fun and games at our dedicated day care center. Our experienced staff ensures a nurturing environment where children can play, learn, and make new friends.
+                    </p>
+                    <p>&nbsp;</p>
+                    <p>
+                      Slides and Trampolines: Dive into a world of pure joy as you slide down thrilling water slides or bounce sky-high on our state-of-the-art trampolines. These exciting activities are perfect for all ages, ensuring endless laughter and excitement for everyone.
+                    </p>
+                    <p>&nbsp;</p>
+                    <p>
+                      Karting: Channel your inner speed demon and satisfy your need for velocity with our exhilarating karting experience. Feel the rush as you navigate hairpin turns and surge ahead to claim victory.
+                    </p>
+                    <p>&nbsp;</p>
+                    <p>
+                      Boat Riding: Set sail on a voyage of discovery and tranquility. Whether you're exploring serene lakes or navigating the open sea, our boat riding services provide a perfect escape, surrounded by breathtaking natural beauty.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="pool-package-wrap col-12">
+                <div class="title-wrap mt-4">
+                  <h5 class="title text-capitalize">Activity adult rates child rates package inclusive</h5>
+                </div>
+                <ul class="package-box mt-3">
+                  <li>Swimming Package KES 300 KES 300 Swimming Package for children includes Water Roller, Boat Riding, Swimming, Trampolines and Slides.</li>
+                  <li>Car Rides N/A KES 200</li>
+                  <li>Zoo KES 150 KES 100</li>
+                  <li>Museum KES 150 KES 100</li>
+                  <li>Zipline Gliding KES 300 N/A</li>
+                  <li>Schools Package KES 300 KES 150 Child rates are inclusive of Swimming, Boat Riding, Slides and Trampolines.</li>
+                </ul>
+                <div class="text-wrap about-text">
+                  <p>
+                    At Springsmatt serene Hotel, we believe in providing exceptional leisure activities that cater to every age and interest. From heart-pounding adventures to calming escapades, we have it all. Book your stay with us now and unlock a world of excitement and relaxation like never before. Your unforgettable adventure awaits!
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-8 col-sm-10">
+            <div class="title-wrap mb-4 title-left">
+              <h4 class="card-title title">Recent listings</h4>
+            </div>
+            <div class="side-box-wrapper">
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./heritage.php"><img src="./assets/offers/caption ().jpg" alt="maasai-mara culture heritage" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Your best heritage & Serene Comfort</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Nestled in the heart of a historically rich landscape, Springsmatt Serene Hotel offers a unique blend of heritage charm and modern luxury.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./accommodation.php"><img src="./assets/offers/caption (3).png" alt="Garden-suite-room" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Top 5 best charming rooms for you</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Our three 50 sq.m perennially popular Garden Suites promise an enchanting oasis in the very heart of the city. </p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./dining.php"><img src="./assets/offers/caption (65).jpg" alt="meals" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Top 10 delicious menus for you</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Embark in a journey through the culinary traditions of the region, with our restaurant serving a delectable array of dishes inspired by historical recipes and local ingredients.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./tour.php"><img src="./assets/captions/tour (15).jpg" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Escape another world with prestige cityscape</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Experience the perfect blend of ambiance, flavor and hospitality.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="box-cat-wrapper">
+              <div class="title-wrap mt-4 title-left pb-4">
+                <h4 class="card-title title">special offers</h4>
+              </div>
+              <div class="box-list-wrap mt-3">
+                <ul class="cat-list">
+                  <li><a href="#">charming rooms experience</a></li>
+                  <li><a href="#">seafood meals at night</a></li>
+                  <li><a href="#">Tranquil Getaway Deal for winter</a></li>
+                  <li><a href="#">Serene Suite Sensation</a></li>
+                  <li><a href="#">prestige cityscape</a></li>
+                  <li><a href="#">Masai mara tour service</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
+<?php
+include("components/footer.html");
+?>
