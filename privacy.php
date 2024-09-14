29b7ecefa4f64072a24690ff51d1fc1a9e989fa9
DevNexus Tec. <160817446+Okinda-joseph@users.noreply.github.com>
2024-09-14 13:31:34 +0300

    Create privacy.php

---
+++ privacy.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- The required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Privacy policy</title>
+  <link rel="stylesheet" href="./css/boostrap.min.css">
+  <link rel="stylesheet" href="./css/boxicons.min.css">
+  <link rel="stylesheet" href="./css/owl.carousel.min.css">
+  <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="./css/styles.css">
+  <script src="https://kit.fontawesome.com/5bebd170da.js" crossorigin="anonymous"></script>
+  <!-- favicon -->
+  <link rel="shortcut icon" href="./assets/icons/logo.jpeg" type="image/x-icon">
+</head>
+
+<body>
+  <?php
+  include("components/header.html");
+  ?>
+  <!-- GENERAL BANNER -->
+  <div class="main-banner-wrapper">
+    <div class="main-banner d-flex align-items-center bg-cover" id="privacy-banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="title-wrap">
+              <h1 class="title display-3 text-capitalize text-white">Privacy policy</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- PRIVACY BODY -->
+  <section id="privacy">
+    <div class="privacy-box-wrapper">
+      <div class="container">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-lg-10">
+            <div class="main-title-wrap mb-4">
+              <h4 class="title">Our hotel policy introduction</h4>
+            </div>
+            <div class="text-wrap about-text privacy-text">
+              <p>
+                Your privacy is important to us, and we are committed in total protection of personal data. This policy shows you how we use your personal data when you interact with us, when we welcome you at our hotel resorts as a guest and when you visit our website for check-in and check-out our availabilities.If you have any questions, please see how to contact us about privacy below... When you are a guest in springsmatt hotels resorts it is good for you to give us your personal data: check-in and check-out attend our hotel resort events interact with our hotel staff and other guests from different parts of the country Log in to our wireless network to enjoy online browsing You may give us data when you interact with us remotely via this website, by post, phone or email.
+              <p>&nbsp;</p>
+              Some of the data collected, as explained above, may be personal data. Personal data means any information about an individual from which that person can be identified. It does not include data where the identity has been removed (anonymous data) below shows the type of personal data we collect that depends on how you interact with us.
+              <p>&nbsp;</p>
+              Identity Data - includes title, first name, middle name, last name, identification documents such as national ID or passport number, nationality, gender, date of birth, images, videos and audio via security cameras in public areas within our properties, username or similar identifier and an encrypted version of your login password. If you interact with us through social media, this may include your social media user name.
+              <p>&nbsp;</p>
+              Contact Data - includes mailing address, email address and telephone numbers. <br> Profile Data - includes your username and encrypted password, your preferences, feedback responses, as well as any data which we have added. <br> Technical Data - includes internet protocol address, your login data, browser type and version, time zone setting and location, platform and other technology on the devices you use to access this website.
+              <br>
+              Usage Data - includes information about how you use our website, products and services. Tracking Data - includes information we collect about you from cookies. Marketing and Communications Data - includes your preferences in receiving direct marketing materials from us.
+              <p>&nbsp;</p>
+              Sensitive personal data – includes but is not limited to: marital status, health, family details which includes the names of both parents and childrenn, their health status and financial information and transaction data. <span>PLEASE NOTE</span> This:, if you choose not to share certain personal data with us, we might not be able to provide the products and services you have asked for.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
+<?php
+include("components/footer.html");
+?>
