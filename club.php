40f5662de2b9fa5d66b21d197317138696b014dc
DevNexus Tec. <160817446+Okinda-joseph@users.noreply.github.com>
2024-09-14 13:24:44 +0300

    Create club.php

---
+++ club.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- The required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Club room</title>
+  <link rel="stylesheet" href="./css/boostrap.min.css">
+  <link rel="stylesheet" href="./css/boxicons.min.css">
+  <link rel="stylesheet" href="./css/owl.carousel.min.css">
+  <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="./css/styles.css">
+  <script src="https://kit.fontawesome.com/5bebd170da.js" crossorigin="anonymous"></script>
+  <!-- favicon -->
+  <link rel="shortcut icon" href="./assets/icons/logo.jpeg" type="image/x-icon">
+</head>
+
+<body>
+  <?php
+  include("components/header.html");
+  ?>
+  <!-- GENERAL BANNER -->
+  <div class="main-banner-wrapper">
+    <div class="main-banner d-flex align-items-center bg-cover" id="club-banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="title-wrap">
+              <h1 class="title display-3 text-capitalize text-white">club room</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- POOL BODY -->
+  <section id="main-content">
+    <div class="content-box-wrapper">
+      <div class="container">
+        <div class="main-title-wrap mb-4">
+          <h4 class="title">Discover club room</h4>
+        </div>
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="row">
+              <div class="col-12">
+                <div class="content-box">
+                  <div class="text-wrap main-text">
+                    <p>
+                      Located on our top floor, our club rooms have a panoramic city skyline view, espresso machine for your coffee indulgence, art deco-style with 26sq.m. Offered on a complimentary basis to all the guests on the club rooms is access to a private executive lounge that is ideally suited to top level meetings The lounge also offers complimentary newspapers and TV coverage of the major news channels, happy hour finger foods and a drink.
+                      Should our guests so desire, breakfast can be served in the lounge.Keeping up with work could not be easier, with a desk and WiFi access available through out the club rooms
+                    </p>
+                    <p>&nbsp;</p>
+                    <div class="main-img-box"><img loading="lazy" src="./assets/captions/room (29).jpg" alt="club-room"></div>
+                    </p>
+                    <p>&nbsp;</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="title-wrap">
+                  <h6 class="title text-capitalize">In the room - amenity</h6>
+                </div>
+                <div class="list-box">
+                  <ul>
+                    <li>Flat screen Tv with satelite channels.</li>
+                    <li>Complimentary wifi access.</li>
+                    <li>Flip flop</li>
+                    <li>Complimentary daily newspapers.</li>
+                    <li>International plug adapters.</li>
+                    <li>Minibar.</li>
+                    <li>24 hours rooms service</li>
+                    <li>Coffee & tea making station</li>
+                    <li>Air and conditioning</li>
+                    <li>safe</li>
+                    <li>Work desks and chairs</li>
+                    <li>Quite room with double glazzed windows</li>
+                    <li>Mosquito reppelant and wipes on requests
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="concel-box">
+                  <div class="title-wrap">
+                    <h6 class="title text-capitalize">Booking concellations</h6>
+                  </div>
+                  <div class="text-wrap about-text">
+                    <p>
+                      When it comes to hotel booking cancellation, the guests are required notify the hotel at least 20 to 24 hours prior to the scheduled check-in time to avoid any charges. Cancellations made within 20 to 24 hours of the reservation will incur a one-night room charge.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="title-wrap pt-3 mb-3 title-left">
+                <h4 class="card-title title">Related rooms</h4>
+              </div>
+              <div class="col-sm-6 mt-3">
+                <div class="card">
+                  <div class="card-img-box">
+                    <a href="./corner.php"><img loading="lazy" src="./assets/captions/room (16).jpg" alt="deluxe room" class="card-img-top"></a>
+                  </div>
+                  <div class="card-img-icons">
+                    <i class="bx bxs-bookmark-heart"></i>
+                    <i class="bx bx-headphone"></i>
+                    <i class="bx bx-wifi-2"></i>
+                    <i class="bx bxs-lock"></i>
+                    <i class="bx bx-key"></i>
+                  </div>
+                  <div class="card-body">
+                    <div class="title-wrap">
+                      <h5 class="card-title title">Corner suite</h5>
+                    </div>
+                    <div class="text-wrap">
+                      <p class="card-text">
+                        Situated on the corner wings of the 5 hotel floors, this elegant Corner suite sprawls across 49 sq. m with an elegant seating area with couches. Enjoy exclusive access to the hotel’s Executive Lounge.</p>
+                    </div>
+                    <div class="card-button-wrap mt-3">
+                      <a href="./book.php" class="book-btn" id="btn">Book now $49 /Night</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6 mt-3">
+                <div class="card">
+                  <div class="card-img-box">
+                    <a href="./garden.php"><img loading="lazy" src="./assets/captions/room (44).jpg" alt="club-room" class="card-img-top"></a>
+                  </div>
+                  <div class="card-img-icons">
+                    <i class="bx bxs-bookmark-heart"></i>
+                    <i class="bx bx-headphone"></i>
+                    <i class="bx bx-wifi-2"></i>
+                    <i class="bx bxs-lock"></i>
+                    <i class="bx bx-key"></i>
+                  </div>
+                  <div class="card-body">
+                    <div class="title-wrap">
+                      <h5 class="card-title title">Garden suite</h5>
+                    </div>
+                    <div class="text-wrap">
+                      <p class="card-text">
+                        Our three 50 sq.m perennially popular Garden Suites promise an enchanting oasis in the very heart of the city. Each Suite opens on to its own private garden terrace. As well as a sunbathing area with sunbed.</p>
+                    </div>
+                    <div class="card-button-wrap mt-3">
+                      <a href="./book.php" class="book-btn" id="btn">Book now $59 /Night</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-8 col-sm-10">
+            <div class="title-wrap mb-4 title-left">
+              <h4 class="card-title title">Recent listings</h4>
+            </div>
+            <div class="side-box-wrapper">
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./heritage.php"><img src="./assets/offers/caption ().jpg" alt="maasai-mara culture heritage" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Your best heritage & Serene Comfort</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Nestled in the heart of a historically rich landscape, Springsmatt Serene Hotel offers a unique blend of heritage charm and modern luxury.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./accommodation.php"><img src="./assets/offers/caption (3).png" alt="Garden-suite-room" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Top 5 best charming rooms for you</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Our three 50 sq.m perennially popular Garden Suites promise an enchanting oasis in the very heart of the city. </p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./dining.php"><img src="./assets/offers/caption (65).jpg" alt="meals" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Top 10 delicious menus for you</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Embark in a journey through the culinary traditions of the region, with our restaurant serving a delectable array of dishes inspired by historical recipes and local ingredients.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./tour.php"><img src="./assets/captions/tour (15).jpg" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Escape another world with prestige cityscape</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Experience the perfect blend of ambiance, flavor and hospitality.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="box-cat-wrapper">
+              <div class="title-wrap mt-4 title-left pb-4">
+                <h4 class="card-title title">special offers</h4>
+              </div>
+              <div class="box-list-wrap mt-3">
+                <ul class="cat-list">
+                  <li><a href="./cottage.php">charming rooms experience</a></li>
+                  <li><a href="">seafood meals at night</a></li>
+                  <li><a href="#">Tranquil Getaway Deal for winter</a></li>
+                  <li><a href="#">Serene Suite Sensation</a></li>
+                  <li><a href="#">prestige cityscape</a></li>
+                  <li><a href="#">Masai mara tour service</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
+<?php
+include("components/footer.html");
+?>
