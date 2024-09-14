8261499e2a4d59e11dc7038282ad7ff01f965d23
DevNexus Tec. <160817446+Okinda-joseph@users.noreply.github.com>
2024-09-14 13:24:37 +0300

    Create conference.php

---
+++ conference.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- The required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Conference</title>
+  <link rel="stylesheet" href="./css/boostrap.min.css">
+  <link rel="stylesheet" href="./css/boxicons.min.css">
+  <link rel="stylesheet" href="./css/owl.carousel.min.css">
+  <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="./css/styles.css">
+  <script src="https://kit.fontawesome.com/5bebd170da.js" crossorigin="anonymous"></script>
+  <!-- favicon -->
+  <link rel="shortcut icon" href="./assets/icons/logo.jpeg" type="image/x-icon">
+</head>
+
+<body>
+  <?php
+  include("components/header.html");
+  ?>
+  <!-- GENERAL BANNER -->
+  <div class="main-banner-wrapper">
+    <div class="main-banner d-flex align-items-center bg-cover" id="conference-banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="title-wrap">
+              <h1 class="title display-3 text-capitalize text-white">Conference</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- CONFERENCE BODY -->
+  <section id="main-content">
+    <div class="content-box-wrapper">
+      <div class="container">
+        <div class="main-title-wrap mb-4">
+          <h4 class="title">Discover best venues for meetings & special events</h4>
+        </div>
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="row">
+              <div class="col-12">
+                <div class="content-box">
+                  <div class="text-wrap about-text main-text">
+                    <p>
+                      Springsmatt Serene Hotel is an ideal venue for meetings, conferences, and special events. Our state-of-the-art facilities, coupled with the historical significance of our venue, provide a unique and inspiring setting for any occasion. Whether you are planning a corporate retreat, a wedding, or a family reunion, our dedicated events team will work with you to ensure every detail is perfect, creating memorable experiences for you and your guests.
+                    </p>
+                    <p>&nbsp;</p>
+                    <div class="main-img-box">
+                      <img loading="lazy" src="./assets/captions/conference (2).jpg" alt="coference-venue-room">
+                    </div>
+                    </p>
+                    <p>&nbsp;</p>
+                  </div>
+                </div>
+              </div>
+              <div class="title-wrap pt-3 mb-3 title-left">
+                <h4 class="card-title title">Related meeting venues</h4>
+              </div>
+              <div class="col-sm-6 mt-3">
+                <div class="card">
+                  <div class="card-img-box">
+                    <a href="./serene.php"><img loading="lazy" src="./assets/captions/conference (14).jpg" alt="serene conference" class="card-img-top" ></a>
+                  </div>
+                  <div class="card-body">
+                    <div class="title-wrap">
+                      <h5 class="card-title title">Serenity conference</h5>
+                    </div>
+                    <div class="text-wrap">
+                      <p class="card-text">
+                      Experience the Serenity Conference, a nurturing environment for personal and professional growth. Rooted in the principles of mindfulness and wellness, this conference offers a holistic approach to leadership development, team building, and organizational success.</p>
+                    </div>
+                    <div class="card-button-wrap mt-3">
+                      <a href="./book.php" class="book-btn" id="btn">Request for proposal</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6 mt-3">
+                <div class="card">
+                  <div class="card-img-box">
+                  <a href="./summit.php"><img loading="lazy" src="./assets/captions/conference (13).jpg" alt="executive conference" class="card-img-top"></a>
+                  </div>
+                  <div class="card-body">
+                    <div class="title-wrap">
+                      <h5 class="card-title title">Executive summit</h5>
+                    </div>
+                    <div class="text-wrap">
+                      <p class="card-text">
+                      Elevate your executive meetings to new heights with our Executive Summit: Strategic Leadership in Action. Designed exclusively for C-suite executives, board members, and senior leaders, this summit offers a premium setting for strategic discussions, decision-making, and collaboration.
+                      </p>
+                    </div>
+                    <div class="card-button-wrap mt-3">
+                      <a href="./book.php" class="book-btn" id="btn">Request for proposal</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-8 col-sm-10">
+            <div class="title-wrap mb-4 title-left">
+              <h4 class="card-title title">Things you may be interested in</h4>
+            </div>
+            <div class="side-box-wrapper">
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                 <a href="./room.php"><img src="./assets/banner/p-banner.jpg" alt="room-cottage" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Your best accommodation suites</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Indulge in comfort, and create cherished memories that will last a lifetime</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./honeymoon.php"><img loading="lazy" src="./assets/captions/wed (2).jpg" alt="coference" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Your best bonquet events</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>
+                      A honeymoons that set the perfect tone for the start of your life together.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./wellness.php"><img loading="lazy" src="./assets/captions/hero (11).jpg" alt="wellness-room" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">The best Massage therapy service</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>
+                    welcome to springsmatt hotel resort spa center, the pinnacle of welliness and relaxation nestled within our hotel resorts.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./dining.php"><img loading="lazy" src="./assets/menu/banner (19).jpg" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Escape another world with your family</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Embark on a culinary journey to the enchanting lands of western kenya.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./gym.php"><img loading="lazy" src="./assets/captions/gym (2).jpg" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Your best wellness center</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Hotel gyms cater to the needs of travelers who want to maintain their fitness regimen during their stay.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="box-cat-wrapper">
+              <div class="title-wrap mt-4 title-left pb-4">
+                <h4 class="card-title title">Categories services</h4>
+              </div>
+              <div class="box-list-wrap mt-3">
+                <ul class="cat-list">
+                  <li><a href="./accommodation.php">Rooms cottage</a></li>
+                  <li><a href="./restaurant.php">Restaurants</a></li>
+                  <li><a href="./room.php">Inspired lodges</a></li>
+                  <li><a href="./wellness.php">spa treatment</a></li>
+                  <li><a href="./tour.php">Travel services</a></li>
+                  <li><a href="./offers.php">special offers</a></li>
+                  <li><a href="./book.php">Bookings</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
+<?php
+include("components/footer.html");
+?>
