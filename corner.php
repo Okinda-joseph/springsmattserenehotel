c63d86e20885a3ca2e2616741fb58f05032f438e
DevNexus Tec. <160817446+Okinda-joseph@users.noreply.github.com>
2024-09-14 13:24:28 +0300

    Create corner.php

---
+++ corner.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- The required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Corner room</title>
+  <link rel="stylesheet" href="./css/boostrap.min.css">
+  <link rel="stylesheet" href="./css/boxicons.min.css">
+  <link rel="stylesheet" href="./css/owl.carousel.min.css">
+  <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="./css/styles.css">
+  <script src="https://kit.fontawesome.com/5bebd170da.js" crossorigin="anonymous"></script>
+  <!-- favicon -->
+  <link rel="shortcut icon" href="./assets/icons/logo.jpeg" type="image/x-icon">
+</head>
+
+<body>
+  <?php
+  include("components/header.html");
+  ?>
+  <!-- GENERAL BANNER -->
+  <div class="main-banner-wrapper">
+    <div class="main-banner d-flex align-items-center bg-cover" id="corner-banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="title-wrap">
+              <h1 class="title display-3 text-capitalize text-white">corner room</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- CORNER BODY -->
+  <section id="main-content">
+    <div class="content-box-wrapper">
+      <div class="container">
+        <div class="main-title-wrap mb-4">
+          <h4 class="title">Discover corner room available</h4>
+        </div>
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="row">
+              <div class="col-12">
+                <div class="content-box">
+                  <div class="text-wrap about-text main-text">
+                    <p>
+                      Situated on the corner wings of the 5 hotel floors, this elegant Corner suite sprawls across 49 sq. m with an elegant seating area with couches.
+
+                      Enjoy exclusive access to the hotel’s Executive Lounge, serving complimentary continental breakfast every day in the morning, 2 happy hour drinks, and snacks during the executive lounge happy hour.
+
+                      Stretch out in this spacious art deco-style room featuring a large bathroom with a separate bathtub, large walk-in shower, and toilet.
+
+                      Catch up with work at the two separate working desks and stay connected with Wi-Fi access.
+                    </p>
+                    <p>&nbsp;</p>
+                    <div class="main-img-box">
+                      <a href="./room.php"><img loading="lazy" src="./assets/captions/room (48).jpg" alt="club-room"></a>
+                    </div>
+                    </p>
+                    <p>&nbsp;</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="title-wrap">
+                  <h6 class="title text-capitalize">In the room - amenity</h6>
+                </div>
+                <div class="list-box">
+                  <ul>
+                    <li>Coffee & tea making facilities</li>
+                    <li>Flat screen Tv with satelite channels</li>
+                    <li>Complimentary wifi access</li>
+                    <li>Coffee marker.</li>
+                    <li>24 hours rooms service</li>
+                    <li>Minibar.</li>
+                    <li>International plug adapters</li>
+                    <li>Air and conditioning</li>
+                    <li>safe</li>
+                    <li>Work desks and chairs</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="concel-box">
+                  <div class="title-wrap">
+                    <h6 class="title text-capitalize">Booking concellations</h6>
+                  </div>
+                  <div class="text-wrap about-text pb-4">
+                    <p>
+                      When it comes to hotel booking cancellation, the guests are required notify the hotel at least 20 to 24 hours prior to the scheduled check-in time to avoid any charges. Cancellations made within 20 to 24 hours of the reservation will incur a one-night room charge.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="title-wrap pt-3 mb-3 title-left">
+                <h4 class="card-title title">Related rooms</h4>
+              </div>
+              <div class="col-sm-6 mt-3">
+                <div class="card">
+                  <div class="card-img-box">
+                    <a href="./club.php"><img loading="lazy" src="./assets/captions/room (29).jpg" alt="deluxe room" class="card-img-top"></a>
+                  </div>
+                  <div class="card-img-icons">
+                    <i class="bx bxs-bookmark-heart"></i>
+                    <i class="bx bx-headphone"></i>
+                    <i class="bx bx-wifi-2"></i>
+                    <i class="bx bxs-lock"></i>
+                    <i class="bx bx-key"></i>
+                  </div>
+                  <div class="card-body">
+                    <div class="title-wrap">
+                      <h5 class="card-title title">Club room</h5>
+                    </div>
+                    <div class="text-wrap">
+                      <p class="card-text">
+                        Located on our top floor, our club rooms have a panoramic city skyline view, espresso machine for your coffee indulgence, art deco-style with 26sq.m.</p>
+                    </div>
+                    <div class="card-button-wrap mt-3">
+                      <a href="./book.php" class="book-btn" id="btn">Book now $49 /Night</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6 mt-3">
+                <div class="card">
+                  <div class="card-img-box">
+                    <a href="./business.php"><img loading="lazy" src="./assets/captions/room (52).jpg" alt="club-room" class="card-img-top"></a>
+                  </div>
+                  <div class="card-img-icons">
+                    <i class="bx bxs-bookmark-heart"></i>
+                    <i class="bx bx-headphone"></i>
+                    <i class="bx bx-wifi-2"></i>
+                    <i class="bx bxs-lock"></i>
+                    <i class="bx bx-key"></i>
+                  </div>
+                  <div class="card-body">
+                    <div class="title-wrap">
+                      <h5 class="card-title title">business suite</h5>
+                    </div>
+                    <div class="text-wrap">
+                      <p class="card-text">
+                        The 58 sq.m Business Suite promises the perfect city base with a view of the lush foliage and canopy of the Processional Way.Classically elegant in style, generous in space the Suite.
+                      </p>
+                    </div>
+                    <div class="card-button-wrap mt-3">
+                      <a href="./book.php" class="book-btn" id="btn">Book now $59 /Night</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-8 col-sm-10">
+            <div class="title-wrap mb-4 title-left">
+              <h4 class="card-title title">New listings</h4>
+            </div>
+            <div class="side-box-wrapper">
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./room.php"><img src="./assets/captions/hero (10).jpg" alt="deluxe room" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Your best family rooms</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Indulge in comfort, and create cherished memories that will last a lifetime</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./coference.php"><img src="./assets/captions/conference (13).jpg" alt="coference" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Your best meeting venue kakamega.</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Nurturing growth experience the serenity conference, a nurturing environment for personal and professional growth.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./dining.php"><img src="./assets/menu/menu (59).png" alt="salad" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Enjoy your best meals</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Embark on a culinary journey to the enchanting lands of western kenya.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./gym.php"><img src="./assets/captions/gym (7).jpg" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Keep body active and healthy while away form home</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Hotel gyms cater to the needs of travelers who want to maintain their fitness regimen during their stay..</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="box-cat-wrapper">
+              <div class="title-wrap mt-4 title-left pb-4">
+                <h4 class="card-title title">Hotel categories services</h4>
+              </div>
+              <div class="box-list-wrap mt-3">
+                <ul class="cat-list">
+                  <li><a href="#">Rooms cottage</a></li>
+                  <li><a href="#">Restaurants</a></li>
+                  <li><a href="#">Inspired lodges</a></li>
+                  <li><a href="#">spa treatment</a></li>
+                  <li><a href="#">Travel services</a></li>
+                  <li><a href="#">Bookings</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
+<?php
+include("components/footer.html");
+?>
