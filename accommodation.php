c2e0e69c90f5d16a771ed0eb511d189ae163224d
DevNexus Tec. <160817446+Okinda-joseph@users.noreply.github.com>
2024-09-14 13:20:32 +0300

    Create accommodation.php

---
+++ accommodation.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- The required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Accommodations</title>
+  <link rel="stylesheet" href="./css/boostrap.min.css">
+  <link rel="stylesheet" href="./css/boxicons.min.css">
+  <link rel="stylesheet" href="./css/owl.carousel.min.css">
+  <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="./css/styles.css">
+  <script src="https://kit.fontawesome.com/5bebd170da.js" crossorigin="anonymous"></script>
+  <!-- favicon -->
+  <link rel="shortcut icon" href="./assets/icons/logo.jpeg" type="image/x-icon">
+</head>
+
+<body>
+  <?php
+  include("components/header.html");
+  ?>
+  <!-- GENERAL BANNER -->
+  <div class="main-banner-wrapper">
+    <div class="main-banner d-flex align-items-center bg-cover" id="accommodation-banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="title-wrap">
+              <h1 class="title display-3 text-capitalize text-white">Accommodations</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ACCOMMADATION BODY -->
+  <section id="accommodation">
+    <div class="accommodation-box-wrapper">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-11 mx-auto mb-4">
+            <div class="accommodation-box">
+              <div class="card accom-box">
+                <div class="card-img-box">
+                  <a href="./deluxe.php">
+                    <img loading="lazy" src="./assets/captions/room (19).jpeg" alt="deluxe-room" class="accom-img-top">
+                  </a>
+                </div>
+                <div class="card-img-icons accom-img-icons">
+                  <i class="bx bxs-bookmark-heart"></i>
+                  <i class="bx bx-headphone"></i>
+                  <i class="bx bx-wifi-2"></i>
+                  <i class="bx bxs-lock"></i>
+                  <i class="bx bx-key"></i>
+                </div>
+                <div class="card-body accom-text">
+                  <div class="title-wrap">
+                    <h5 class="card-title title">Deluxe rooms</h5>
+                  </div>
+                  <div class="text-wrap body-text">
+                    <p class="card-text">
+                      Amenities include complimentary high-speed Wi-Fi, individually controlled climate control, 24-hour room service, and daily housekeeping with fresh linens and towels. An in-room safe ensures the security of your valuables. Additional services feature a dedicated concierge team, laundry and dry-cleaning services, and complimentary access to the fitness center and spa facilities. The hotel’s serene and picturesque setting offers a peaceful retreat while being conveniently close to local attractions and transport links, making it perfect for both leisure and business travelers.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-11 mx-auto mb-4">
+            <div class="accommodation-box">
+              <div class="card accom-box">
+                <div class="card-img-box">
+                  <a href="./club.php">
+                    <img loading="lazy" src="./assets/captions/room (29).jpg" alt="club-room" class="accom-img-top">
+                  </a>
+                </div>
+                <div class="card-img-icons accom-img-icons">
+                  <i class="bx bxs-bookmark-heart"></i>
+                  <i class="bx bx-headphone"></i>
+                  <i class="bx bx-wifi-2"></i>
+                  <i class="bx bxs-lock"></i>
+                  <i class="bx bx-key"></i>
+                </div>
+                <div class="card-body accom-text">
+                  <div class="title-wrap">
+                    <h5 class="card-title title">Club room</h5>
+                  </div>
+                  <div class="text-wrap body-text">
+                    <p class="card-text">
+                      Located on our top floor, our club rooms have a panoramic city skyline view, espresso machine for your coffee indulgence, art deco-style with 26sq.m. Offered on a complimentary basis to all the guests on the club rooms is access to a private executive lounge that is ideally suited to top level meetings The lounge also offers complimentary newspapers and TV coverage of the major news channels, happy hour finger foods and a drink.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-11 mx-auto mb-4">
+            <div class="accommodation-box">
+              <div class="card accom-box">
+                <div class="card-img-box">
+                  <a href="./superior.php">
+                    <img loading="lazy" src="./assets/captions/room (47).jpg" alt="superior-room" class="accom-img-top">
+                  </a>
+                </div>
+                <div class="card-img-icons accom-img-icons">
+                  <i class="bx bxs-bookmark-heart"></i>
+                  <i class="bx bx-headphone"></i>
+                  <i class="bx bx-wifi-2"></i>
+                  <i class="bx bxs-lock"></i>
+                  <i class="bx bx-key"></i>
+                </div>
+                <div class="card-body accom-text">
+                  <div class="title-wrap">
+                    <h5 class="card-title title">Superior room</h5>
+                  </div>
+                  <div class="text-wrap body-text">
+                    <p class="card-text">
+                      Our spacious and elgantly presented superior rooms of 44sq.m have king-sized beds and private verandahs. The lavish bathrooms offer both baths and showers. All rooms enjoy panoramic views over the city scape. Our air conditioned rooms have large double glazed window panels with WIFI internet connection.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-11 mx-auto mb-4">
+            <div class="accommodation-box">
+              <div class="card accom-box">
+                <div class="card-img-box">
+                  <a href="./corner.php"><img loading="lazy" src="./assets/captions/room (48).jpg" alt="Corner-room" class="accom-img-top"></a>
+                </div>
+                <div class="card-img-icons accom-img-icons">
+                  <i class="bx bxs-bookmark-heart"></i>
+                  <i class="bx bx-headphone"></i>
+                  <i class="bx bx-wifi-2"></i>
+                  <i class="bx bxs-lock"></i>
+                  <i class="bx bx-key"></i>
+                </div>
+                <div class="card-body accom-text">
+                  <div class="title-wrap">
+                    <h5 class="card-title title">Corner suites</h5>
+                  </div>
+                  <div class="text-wrap body-text">
+                    <p class="card-text">
+                      Situated on the corner wings of the 5 hotel floors, this elegant Corner suite sprawls across 49 sq. m with an elegant seating area with couches. Enjoy exclusive access to the hotel’s Executive Lounge, serving complimentary continental breakfast every day in the morning, 2 happy hour drinks, and snacks during the executive lounge happy hour. Stretch out in this spacious art deco-style room featuring a large bathroom with a separate bathtub, large walk-in shower, and toilet.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-11 mx-auto mb-4">
+            <div class="accommodation-box">
+              <div class="card accom-box">
+                <div class="card-img-box">
+                  <a href="./garden.php"><img loading="lazy" src="./assets/captions/room (49).jpg" alt="Garden-room" class="accom-img-top"></a>
+                </div>
+                <div class="card-img-icons accom-img-icons">
+                  <i class="bx bxs-bookmark-heart"></i>
+                  <i class="bx bx-headphone"></i>
+                  <i class="bx bx-wifi-2"></i>
+                  <i class="bx bxs-lock"></i>
+                  <i class="bx bx-key"></i>
+                </div>
+                <div class="card-body accom-text">
+                  <div class="title-wrap">
+                    <h5 class="card-title title">Garden suite</h5>
+                  </div>
+                  <div class="text-wrap body-text">
+                    <p class="card-text">
+                      Our three 50 sq.m perennially popular Garden Suites promise an enchanting oasis in the very heart of the city. Each Suite opens on to its own private garden terrace. As well as a sunbathing area with sunbed. Each private garden also features a covered pergola, which serves as a Mediterranean-styled private dining area. A private gate opens on to the tropical gardens of the hotel.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-11 mx-auto mb-4">
+            <div class="accommodation-box">
+              <div class="card accom-box">
+                <div class="card-img-box">
+                  <a href="./executive.php"> <img loading="lazy" src="./assets/captions/room (41).jpg" alt="executive-room" class="accom-img-top"></a>
+                </div>
+                <div class="card-img-icons accom-img-icons">
+                  <i class="bx bxs-bookmark-heart"></i>
+                  <i class="bx bx-headphone"></i>
+                  <i class="bx bx-wifi-2"></i>
+                  <i class="bx bxs-lock"></i>
+                  <i class="bx bx-key"></i>
+                </div>
+                <div class="card-body accom-text">
+                  <div class="title-wrap">
+                    <h5 class="card-title title">Executive suite</h5>
+                  </div>
+                  <div class="text-wrap body-text">
+                    <p class="card-text">
+                      Located on our high floors our 51 sq.m Executive suites are sophisticated in style, spacious in aspect and intuitively appointed to provide either an executive working and reception space, or the ultimate in romantic venues. They offer a sumptuous king-sized bed, an elegant seating area with couches and a writing desk. Add to this a lavish bathroom with bath and shower and you have the optimum city base. All our guests have access to our Executive Lounge where they can enjoy breakfast in the morning and happy hour snacks and a drink.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-11 mx-auto mb-4">
+            <div class="accommodation-box">
+              <div class="card accom-box">
+                <div class="card-img-box">
+                  <a href="./business.php"> <img loading="lazy" src="./assets/captions/room (52).jpg" alt="business-room" class="accom-img-top"></a>
+                </div>
+                <div class="card-img-icons accom-img-icons">
+                  <i class="bx bxs-bookmark-heart"></i>
+                  <i class="bx bx-headphone"></i>
+                  <i class="bx bx-wifi-2"></i>
+                  <i class="bx bxs-lock"></i>
+                  <i class="bx bx-key"></i>
+                </div>
+                <div class="card-body accom-text">
+                  <div class="title-wrap">
+                    <h5 class="card-title title">Business suite</h5>
+                  </div>
+                  <div class="text-wrap body-text">
+                    <p class="card-text">
+                      The 58 sq.m Business Suite promises the perfect city base with a view of the lush foliage and canopy of the Processional Way. Classically elegant in style, generous in space the Suite comprises of a separate bedroom with sumptuous king-sized bed, a lavish bathroom with bath and shower. An art deco-style sophisticated lounge (equipped with sofas, TV and writing desk); a dining area (with seating for four), a guest washroom. All our guests have access to the Executive Lounge.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-11 mx-auto mb-4">
+            <div class="accommodation-box">
+              <div class="card accom-box">
+                <div class="card-img-box">
+                  <a href="./state.php"> <img loading="lazy" src="./assets/captions/room (51).jpg" alt="state-room" class="accom-img-top"></a>
+                </div>
+                <div class="card-img-icons accom-img-icons">
+                  <i class="bx bxs-bookmark-heart"></i>
+                  <i class="bx bx-headphone"></i>
+                  <i class="bx bx-wifi-2"></i>
+                  <i class="bx bxs-lock"></i>
+                  <i class="bx bx-key"></i>
+                </div>
+                <div class="card-body accom-text">
+                  <div class="title-wrap">
+                    <h5 class="card-title title">State suite</h5>
+                  </div>
+                  <div class="text-wrap body-text">
+                    <p class="card-text">
+                      Impressive indeed, the magnificent State Suite is our largest suite measuring 165 sq. m located on the hotel top floor . It has been designed to offer the ultimate comfort, exclusivity, and privacy. Ideally suited to both private entertainment and exclusive business gatherings, it features a private entrance lobby for the reception of guests, an opulent drawing room (with audio system and plasma TV), a private bar, a private dining room with its own kitchenette, and a guest washroom.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-11 mx-auto mb-4">
+            <div class="accommodation-box">
+              <div class="card accom-box">
+                <div class="card-img-box">
+                  <a href="./cottage.php"><img loading="lazy" src="./assets/captions/room (32).jpg" alt="family-room" class="accom-img-top"></a>
+                </div>
+                <div class="card-img-icons accom-img-icons">
+                  <i class="bx bxs-bookmark-heart"></i>
+                  <i class="bx bx-headphone"></i>
+                  <i class="bx bx-wifi-2"></i>
+                  <i class="bx bxs-lock"></i>
+                  <i class="bx bx-key"></i>
+                </div>
+                <div class="card-body accom-text">
+                  <div class="title-wrap">
+                    <h5 class="card-title title">Family cottage room</h5>
+                  </div>
+                  <div class="text-wrap body-text">
+                    <p class="card-text">
+                      Welcome to our charming cottage room inside our hotel, where tranquility and comfort await you. Nestled amidst breathtaking natural beauty, our cozy cottages offer a serene retreat for those seeking a peaceful getaway.
+                      The charming cottage rooms have a high air conditioner, high speed WiFis strong Locker Breakfast smart security cleaning24/7 Online support single bed</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-11 mx-auto">
+            <div class="main-icon-wrap-box text-center">
+              <div class="icon-wrap-1">
+                <i class='bx bx-bookmark-heart'></i>
+                <p>Hotel bookmark</p>
+              </div>
+              <div class="icon-wrap-1">
+                <i class='bx bx-headphone'></i>
+                <p>International 24-hour services restaurant</p>
+              </div>
+              <div class="icon-wrap-1">
+                <i class='bx bx-wifi-2'></i>
+                <p>Flat screen TV with satellite channels and Wi-Fi.</p>
+              </div>
+              <div class="icon-wrap-1">
+                <i class='bx bx-lock'></i>
+                <p>Adequate security lock</p>
+
+              </div>
+              <div class="icon-wrap-1">
+                <i class='bx bx-key'></i>
+                <p>Security paramount</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-11 mx-auto mt-3">
+            <div class="list-wrap">
+              <div class="title-wrap">
+                <h6 class="title text-capitalize">*terms & condition*</h6>
+              </div>
+              <div class="list-box">
+                <ul>
+                  <li>Browse springsmatt website regularly to see new listings every week</li>
+                  <li>The Check-in time is from 4:00PM, check-out by 9:00AM.</li>
+                  <li>Regular car parking service available</li>
+                  <li>Observe quiet hours, 10:30PM to 4:30 AM.</li>
+                  <li>The discount applicable on every online accommodation Booking done the SpringsMattSerene hotel website.</li>
+                  <li>The offers applicable for bookings done for our SpringsMatt serene hotel.</li>
+                </ul>
+              </div>
+            </div>
+            <div class="concel-box mt-4">
+              <div class="title-wrap">
+                <h6 class="title text-capitalize">Booking concellations</h6>
+              </div>
+              <div class="text-wrap about-text">
+                <p>
+                  When it comes to hotel booking cancellation, the guests are required notify the hotel at least 20 to 24 hours prior to the scheduled check-in time to avoid any charges. <br> Cancellations made within 20 to 24 hours of the reservation will incur a one-night room charge.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
+<?php
+include("components/footer.html");
+?>
