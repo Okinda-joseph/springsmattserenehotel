6729c69955ac2e638c327d9260aa21b1e7614bec
DevNexus Tec. <160817446+Okinda-joseph@users.noreply.github.com>
2024-09-14 13:35:42 +0300

    Create gym.php

---
+++ gym.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- The required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Health club</title>
+  <link rel="stylesheet" href="./css/boostrap.min.css">
+  <link rel="stylesheet" href="./css/boxicons.min.css">
+  <link rel="stylesheet" href="./css/owl.carousel.min.css">
+  <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="./css/styles.css">
+  <script src="https://kit.fontawesome.com/5bebd170da.js" crossorigin="anonymous"></script>
+  <!-- favicon -->
+  <link rel="shortcut icon" href="./assets/icons/logo.jpeg" type="image/x-icon">
+</head>
+
+<body>
+  <?php
+  include("components/header.html");
+  ?>
+  <!-- GENERAL BANNER -->
+  <div class="main-banner-wrapper">
+    <div class="main-banner d-flex align-items-center bg-cover" id="healthclub-banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="title-wrap">
+              <h1 class="title display-3 text-capitalize text-white">health club</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- WELLNESS BODY -->
+  <section id="main-content">
+    <div class="content-box-wrapper">
+      <div class="container">
+        <div class="main-title-wrap mb-4">
+          <h4 class="title">Our exclusive health club</h4>
+        </div>
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="row">
+              <div class="col-12">
+                <div class="content-box">
+                  <div class="text-wrap main-text">
+                    <p>
+                      A springsMatt serene hotel health club is a comprehensive facility within our hotel that provides guests with access to a wide range of fitness amenities and wellness services. Unlike a standard hotel gym, a health club typically offers a more extensive array of features aimed at promoting holistic well-being.
+                    </p>
+                    <p>&nbsp;</p>
+                    <p>
+                      Springsmatt serene hotel health club enhances all of our guests with experience by providing a comprehensive wellness facility that promotes physical fitness, mental relaxation, and overall well-being. It serves as a valuable amenity for travelers seeking a balanced and rejuvenating stay, ensuring that they can maintain their health and vitality even while on the road.
+                    </p>
+                    <p>&nbsp;</p>
+                    <p>
+                    <div class="main-img-box">
+                      <img loading="lazy" src="./assets/captions/gym (5).webp" alt="fitnes-hall-center">
+                    </div>
+                    </p>
+                    <p>&nbsp;</p>
+                    <p>
+                      our hotel has employ certified fitness instructors, personal trainers, and wellness experts to assist guests in achieving their fitness goals and addressing their specific needs. Hotel health clubs cater to a diverse range of guests, from fitness enthusiasts looking for high-intensity workouts to travelers seeking relaxation and stress relief.
+                      They offer a sanctuary within the hotel where guests can prioritize their health and well-being, whether through invigorating exercise sessions, soothing spa treatments, or simply unwinding in a tranquil environment
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-8 col-sm-10">
+            <div class="title-wrap mb-4 title-left">
+              <h4 class="card-title title">New listings</h4>
+            </div>
+            <div class="side-box-wrapper">
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./heritage.php"><img loading="lazy" src="./assets/menu/banner (14).jpg" alt="culture-heritage" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Your best heritage & Serene Comfort</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Nestled in the heart of a historically rich landscape, Springsmatt Serene Hotel offers a unique blend of heritage charm and modern luxury.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./accommodation.php"><img loading="lazy" src="./assets/blog/blog (178).jpg" alt="Garden-suite-room" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Top 9 best charming rooms for you</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Our three 50 sq.m perennially popular Garden Suites promise an enchanting oasis in the very heart of the city. </p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./dining.php"><img loading="lazy" src="./assets/menu/banner (5).jpg" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Top 10 delicious menus for you</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Embark in a journey through the culinary traditions of the region, with our restaurant serving a delectable array of dishes inspired by historical recipes and local ingredients.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./prestige.php"><img loading="lazy" src="./assets/menu/banner (23).jpg" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Escape another world with prestige cityscape</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Experience the perfect blend of ambiance, flavor and hospitality.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="box-cat-wrapper">
+              <div class="title-wrap mt-4 title-left pb-4">
+                <h4 class="card-title title">special offers</h4>
+              </div>
+              <div class="box-list-wrap mt-3">
+                <ul class="cat-list">
+                  <li><a href="#">charming rooms experience</a></li>
+                  <li><a href="#">seafood meals at night</a></li>
+                  <li><a href="#">Tranquil Getaway Deal for winter</a></li>
+                  <li><a href="#">Serene Suite Sensation</a></li>
+                  <li><a href="#">prestige cityscape</a></li>
+                  <li><a href="#">Masai mara tour service</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
+<?php
+include("components/footer.html");
+?>
