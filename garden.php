15c4cd336b116fbfe1d8471d1278316ec42d8b00
DevNexus Tec. <160817446+Okinda-joseph@users.noreply.github.com>
2024-09-14 13:35:50 +0300

    Create garden.php

---
+++ garden.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- The required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Garden suite</title>
+  <link rel="stylesheet" href="./css/boostrap.min.css">
+  <link rel="stylesheet" href="./css/boxicons.min.css">
+  <link rel="stylesheet" href="./css/owl.carousel.min.css">
+  <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="./css/styles.css">
+  <script src="https://kit.fontawesome.com/5bebd170da.js" crossorigin="anonymous"></script>
+  <!-- favicon -->
+  <link rel="shortcut icon" href="./assets/icons/logo.jpeg" type="image/x-icon">
+</head>
+
+<body>
+  <?php
+  include("components/header.html");
+  ?>
+  <!-- GENERAL BANNER -->
+  <div class="main-banner-wrapper">
+    <div class="main-banner d-flex align-items-center bg-cover" id="garden-banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="title-wrap">
+              <h1 class="title display-3 text-capitalize text-white">Garden suite</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--  EXECUTIVE BODY -->
+  <section id="main-content">
+    <div class="content-box-wrapper">
+      <div class="container">
+        <div class="main-title-wrap mb-4">
+          <h4 class="title">Discover garden suite available</h4>
+        </div>
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="row">
+              <div class="col-12">
+                <div class="content-box">
+                  <div class="text-wrap about-text main-text">
+                    <p>
+                      Our three 50 sq.m perennially popular Garden Suites promise an enchanting oasis in the very heart of the city. Each Suite opens on to its own private garden terrace. As well as a sunbathing area with sunbed.
+
+                      Each private garden also features a covered pergola, which serves as a Mediterranean-styled private dining area. A private gate opens on to the tropical gardens of the hotel.
+                    </p>
+                    <p>&nbsp;</p>
+                    <p>
+                      The ideal venue for a romantic weekend, the Suites also provide the optimum executive base for those wishing to conduct business in khwisero kakamega county.Each room offers, a bedroom with king-sized bed, a spacious seating area with writing desk and a lavishly appointed bathroom with bath and shower. All our guests have access to the executive lounge.
+                    </p>
+                    <p>&nbsp;</p>
+                    <div class="main-img-box">
+                      <a href="./accommodation.php"><img loading="lazy" src="./assets/captions/room (49).jpg" alt="club-room"></a>
+                    </div>
+                    </p>
+                    <p>&nbsp;</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="title-wrap">
+                  <h6 class="title text-capitalize">In the room - amenity</h6>
+                </div>
+                <div class="list-box">
+                  <ul>
+                    <li>Flat screen Tv with satelite channels</li>
+                    <li>Complimentary wireless internet access</li>
+                    <li>Free daily newspapers</li>
+                    <li>Flip flop</li>
+                    <li>Quite rooms with double glazzed windowws</li>
+                    <li>Work desks and chairs</li>
+                    <li>International plug adapter</li>
+                    <li>Guest bathroom excepts private bathroom</li>
+                    <li>Air conditioning</li>
+                    <li>24 hours room service</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="concel-box">
+                  <div class="title-wrap">
+                    <h6 class="title text-capitalize">Booking concellations</h6>
+                  </div>
+                  <div class="text-wrap about-text pb-4">
+                    <p>
+                      When it comes to hotel booking cancellation, the guests are required notify the hotel at least 20 to 24 hours prior to the scheduled check-in time to avoid any charges. Cancellations made within 20 to 24 hours of the reservation will incur a one-night room charge.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="title-wrap pt-3 mb-3 title-left">
+                <h4 class="card-title title">Related suites</h4>
+              </div>
+              <div class="col-sm-6 mt-3">
+                <div class="card">
+                  <div class="card-img-box">
+                    <a href="./garden.php"><img loading="lazy" src="./assets/captions/room (52).jpg" alt="deluxe-single-room" class="card-img-top"></a>
+                  </div>
+                  <div class="card-img-icons">
+                    <i class="bx bxs-bookmark-heart"></i>
+                    <i class="bx bx-headphone"></i>
+                    <i class="bx bx-wifi-2"></i>
+                    <i class="bx bxs-lock"></i>
+                    <i class="bx bx-key"></i>
+                  </div>
+                  <div class="card-body">
+                    <div class="title-wrap">
+                      <h5 class="card-title title">Business suite</h5>
+                    </div>
+                    <div class="text-wrap">
+                      <p class="card-text">
+                        The 58 sq.m Business Suite promises the perfect city base with a view of the lush foliage and canopy of the Processional Way. Classically elegant in style, generous in space the Suite comprises of a separate bedroom.</p>
+                    </div>
+                    <div class="card-button-wrap mt-3">
+                      <a href="./book.php" class="book-btn" id="btn">Book now $72 /Night</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6 mt-3">
+                <div class="card">
+                  <div class="card-img-box">
+                    <a href="./state.php"><img loading="lazy" src="./assets/captions/room (51).jpg" alt="executive-deluxe-room" class="card-img-top"></a>
+                  </div>
+                  <div class="card-img-icons">
+                    <i class="bx bxs-bookmark-heart"></i>
+                    <i class="bx bx-headphone"></i>
+                    <i class="bx bx-wifi-2"></i>
+                    <i class="bx bxs-lock"></i>
+                    <i class="bx bx-key"></i>
+                  </div>
+                  <div class="card-body">
+                    <div class="title-wrap">
+                      <h5 class="card-title title">state suite</h5>
+                    </div>
+                    <div class="text-wrap">
+                      <p class="card-text">
+                        Impressive indeed, the magnificent State Suite is our largest suite measuring 165 sq. m located on the hotel top floor . It has been designed to offer the ultimate comfort, exclusivity, and privacy, both private and business gatherings.
+                      </p>
+                    </div>
+                    <div class="card-button-wrap mt-3">
+                      <a href="./book.php" class="book-btn" id="btn">Book now $74 /Night</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-8 col-sm-10">
+            <div class="title-wrap mb-4 title-left">
+              <h4 class="card-title title">New listings</h4>
+            </div>
+            <div class="side-box-wrapper">
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./room.php"><img loading="lazy" src="./assets/captions/room (32).jpg" alt="deluxe room" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Your best accommodation suites</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Rooms suites where comfort meets luxury in perfect harmony. Designed with your utmost relaxation and enjoyment in mind, this spacious haven is ideal for creating lasting memories with your loved ones.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./coference.php"><img loading="lazy" src="./assets/captions/conference (14).jpg" alt="coference" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Your best meeting venues</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Nurturing growth experience the serenity conference, a nurturing environment for personal and professional growth.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./dining.php"><img loading="lazy" src="./assets/menu/menu (59).png" alt="salad" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Enjoy your best meals</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Embark on a culinary journey to the enchanting lands of western kenya.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./gym.php"><img loading="lazy" src="./assets/captions/gym (7).jpg" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Keep body active and healthy while away form home</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Hotel gyms cater to the needs of travelers who want to maintain their fitness regimen during their stay..</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="box-cat-wrapper">
+              <div class="title-wrap mt-4 title-left pb-4">
+                <h4 class="card-title title">categories services</h4>
+              </div>
+              <div class="box-list-wrap mt-3">
+                <ul class="cat-list">
+                  <li><a href="./accommodation.php">Rooms cottage</a></li>
+                  <li><a href="./restaurant.php">Restaurants</a></li>
+                  <li><a href="./room.php">Inspired lodges</a></li>
+                  <li><a href="./wellness.php">spa treatment</a></li>
+                  <li><a href="./tour.php">Travel services</a></li>
+                  <li><a href="./book.php">Bookings</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
+<?php
+include("components/footer.html");
+?>
