f6934f51e8c545a956f916e423107dd2b5cb3fd1
DevNexus Tec. <160817446+Okinda-joseph@users.noreply.github.com>
2024-09-14 13:08:56 +0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- The required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>About | springsmatt</title>
+  <link rel="stylesheet" href="./css/boostrap.min.css">
+  <link rel="stylesheet" href="./css/boxicons.min.css">
+  <link rel="stylesheet" href="./css/owl.carousel.min.css">
+  <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="./css/styles.css">
+  <script src="https://kit.fontawesome.com/5bebd170da.js" crossorigin="anonymous"></script>
+  <!-- favicon -->
+  <link rel="shortcut icon" href="./assets/icons/logo.jpeg" type="image/x-icon">
+</head>
+
+<body>
+  <?php
+  include("components/header.html");
+  ?>
+  <!-- BANNERSS -->
+  <div id="about-main-banner">
+    <div class="about-box bg-cover d-flex align-items-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="title-wrap">
+              <h1 class="title display-3 text-capitalize text-white">About us</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- BODY -->
+  <section id="about-body">
+    <div class="about-body-box">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="row">
+              <div class="col-12">
+                <div class="text-wrap">
+                  <p class="about-text">
+                    Springsmatt serene hotel rooms and suites are a harmonious blend of historical charm and modern sophistication. Each space is thoughtfully designed to provide the utmost comfort, featuring period furnishings, vintage decor, and all the contemporary amenities needed for a restful stay. Whether you choose a cozy room or an expensive suite, you’ll find a perfect retreat that reflects the timeless elegance of Springsmatt. Dining at Springsmatt Serene Hotel is an experience in itself. Our restaurant offers a delectable array of dishes inspired by traditional recipes and crafted with locally sourced ingredients. From hearty breakfasts to elegant dinners, our culinary team is dedicated to providing a dining experience that celebrates the rich flavors and heritage of the region. Enjoy your meals in an ambiance that combines historical charm with refined elegance.
+                  </p>
+                  <p>&nbsp;</p>
+                  <p>
+                  <div class="about-main-img"><img loading="lazy" src="./assets/captions/room (18).jpg" alt="room"></div>
+                  </p>
+                  <p>&nbsp;</p>
+                  <p class="about-text">
+                    Springsmatt Serene Hotel is an ideal venue for meetings, conferences, and special events. Our state-of-the-art facilities, coupled with the historical significance of our venue, provide a unique and inspiring setting for any occasion. Whether you are planning a corporate retreat, a wedding, or a family reunion, our dedicated events team will work with you to ensure every detail is perfect, creating memorable experiences for you and your guests. We are pleased to offer a range of exclusive packages and promotions designed to enhance your stay at Springsmatt Serene Hotel. From romantic getaways to cultural exploration packages, our offers provide unique opportunities to experience the best of what our hotel and the surrounding region have to offer. Be sure to check our website regularly for the latest deals and special offers.
+                  </p>
+                  <p>&nbsp;</p>
+                  <p class="about-text mb-3">
+                    At Springsmatt Serene Hotel, we value your privacy and are committed to protecting your personal information. Our privacy policy outlines how we collect, use, and safeguard your data to ensure your peace of mind during your stay with us. Additionally, our terms and conditions provide detailed information about your booking, stay, and use of our services. We encourage you to review these documents to fully understand your rights and responsibilities as a guest. Springsmatt Serene Hotel is more than just a place to stay; it’s a destination that offers a rich tapestry of historical and cultural experiences. We invite you to explore our heritage, relax in our serene environment, and create lasting memories. Under the leadership of Mr. Joseph were, our dedicated team is committed to making your stay with us exceptional in every way.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-8 col-sm-10">
+            <div class="title-wrap mb-4">
+              <h4 class="card-title title">Springsmatt hotel members</h4>
+            </div>
+            <div class="side-box-wrapper member-box">
+              <div class="box-wrap pt-4 pb-4">
+                <div class="about-img-wrap">
+                  <img loading="lazy" decoding="async" src="./assets/banner/hero/hero (1).png" alt="hotel-general-supervisor" class="about-img-top">
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Mrs.Desmond kemboi</h6>
+                  </div>
+                  <div class="box-text">
+                    <p class="text-capitalize">The supervisor</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-4 pb-4">
+                <div class="about-img-wrap">
+                  <img loading="lazy" decoding="async" src="./assets/banner//hero/hero (1).jpg" alt="digital-marketing" class="about-img-top">
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Mr.Miracle kamau</h6>
+                  </div>
+                  <div class="box-text">
+                    <p class="text-capitalize">Digital marketing manager</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-4 pb-4">
+                <div class="about-img-wrap">
+                  <img loading="lazy" decoding="async" src="./assets/banner/hero/hero (2).png" alt="manager" class="about-img-top">
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Mrs. Miriam miwa</h6>
+                  </div>
+                  <div class="box-text">
+                    <p class="text-capitalize">The General manager</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-4 pb-4">
+                <div class="about-img-wrap">
+                  <img loading="lazy" decoding="async" src="./assets/banner/hero/hero (1).jfif" alt="CEO" class="about-img-top">
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Mr.joseph were</h6>
+                  </div>
+                  <div class="box-text">
+                    <p class="text-capitalize">The hotel CEO/president</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- CORE FEATURES -->
+  <section id="core" class="bg-light">
+    <div class="core-wrapper">
+      <div class="container">
+        <div class="main-title-wrap">
+          <h4 class="title text-center mb-4">Core features</h4>
+        </div>
+        <div class="row">
+          <div class="col-lg-8 mt-4 mx-auto">
+            <div class="row g-4 justify-content-center">
+              <div class="col-sm-6">
+                <div class="core-wrapper-box">
+                  <div class="core-box">
+                    <div class="core-img-box">
+                      <img src="./assets/icons/icons (5).png" alt="" class="core-img-top mb-3">
+                    </div>
+                    <div class="core-body-box">
+                      <div class="titile-wrap">
+                        <h6 class="title text-center">Rooms</h6>
+                      </div>
+                      <div class="text-wrap">
+                        <p class="card-text">
+                          Welcome to our exquisite deluxe, superior and garden suite rooms, where comfort meets luxury in perfect harmony. Designed with your utmost relaxation and enjoyment in mind, this spacious haven is ideal for creating lasting memories that will last lifetimes.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="core-wrapper-box">
+                  <div class="core-box">
+                    <div class="core-img-box">
+                      <img src="./assets/icons/icons (2).png" alt="" class="core-img-top mb-3">
+                    </div>
+                    <div class="core-body-box">
+                      <div class="titile-wrap">
+                        <h6 class="title text-center">restaurant</h6>
+                      </div>
+                      <div class="text-wrap">
+                        <p class="card-text">
+                          Culinary excellence takes center stage as talented chefs craft a menu that celebrates both local flavors and international influences. Each dish is a masterpiece, meticulously prepared using the finest, freshest ingredients sourced from local markets or specialty suppliers.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="core-wrapper-box">
+                  <div class="core-box">
+                    <div class="core-img-box">
+                      <img src="./assets/icons/icons (4).png" alt="" class="core-img-top mb-3">
+                    </div>
+                    <div class="core-body-box">
+                      <div class="titile-wrap">
+                        <h6 class="title text-center">swimming pool</h6>
+                      </div>
+                      <div class="text-wrap">
+                        <p class="card-text">
+                          Our swimming pools are meticulously maintained to ensure crystal-clear waters that beckon you to take a dip. Dive into a world of serenity as our professionally trained lifeguards prioritize your safety while you savor every moment. Whether you're seeking a peaceful swim or a fun-filled family adventure, our pools cater to all your desires.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="core-wrapper-box">
+                  <div class="core-box">
+                    <div class="core-img-box">
+                      <img src="./assets/icons/icons (3).png" alt="Map-location" class="core-img-top mb-3">
+                    </div>
+                    <div class="core-body-box">
+                      <div class="titile-wrap">
+                        <h6 class="title text-center">Tour services</h6>
+                      </div>
+                      <div class="text-wrap">
+                        <p class="card-text">
+                          Imagine yourself standing amidst the vast savannah, observing majestic lions, graceful cheetahs, and towering giraffes in their natural habitat. Our experienced guides, with their in-depth knowledge and passion for wildlife, will ensure you don't miss a moment of this breathtaking beauty and lifetimes.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--  BLOG -->
+  <section id="listings">
+    <div class="container">
+      <div class="main-title-wrap mb-4">
+        <h4 class="title">Blogs</h4>
+      </div>
+      <div class="title-wrap mb-4 title-left">
+        <h4 class="card-title title">Recents posts</h4>
+      </div>
+      <div class="row g-4">
+        <div class="col-lg-4 col-sm-6">
+          <div class="card">
+            <div class="card-img-box">
+              <a href="#"><img loading="lazy" src="./assets/captions/room (52).jpg" class="card-img-top" alt="family-room"></a>
+            </div>
+            <div class="card-body">
+              <div class="title-wrap">
+                <h5 class="card-title title">Family room cottage</h5>
+              </div>
+              <div class="text-wrap">
+                <p class="card-text">
+                  Family cottage, where comfort meets luxury in perfect harmony. Designed with your utmost relaxation and enjoyment in mind, this spacious haven is ideal for creating lasting memories with your loved ones.
+                </p>
+              </div>
+              <div class="button-wrap mt-3"><a class="btn-text" href="#">Learn more</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <div class="card">
+            <div class="card-img-box">
+              <a href="#"><img src="./assets/captions/conference (2).jpg" class="card-img-top" alt="coference-summit"></a>
+            </div>
+            <div class="card-body">
+              <div class="title-wrap">
+                <h5 class="card-title title">Serenity conference</h5>
+              </div>
+              <div class="text-wrap">
+                <p class="card-text">
+                  Springsmatt is an ideal venue for meetings, conferences, and special events. Our state-of-the-art facilities, coupled with the historical significance of our venue, provide a unique and inspiring setting for any occasion
+                </p>
+              </div>
+              <div class="button-wrap mt-3"><a class="btn-text" href="#">Learn more</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <div class="card">
+            <div class="card-img-box">
+              <a href="#"><img src="./assets/captions/spa (5).jpg" class="card-img-top" alt="message"></a>
+            </div>
+            <div class="card-body">
+              <div class="title-wrap">
+                <h5 class="card-title title">spa health club</h5>
+              </div>
+              <div class="text-wrap">
+                <p class="card-text">
+                  Hotel health club provides guests with access to a wide range of fitness amenities and wellness services. Unlike a standard hotel gym, a health club typically offers a more extensive array of features aimed at promoting holistic well-being.
+                </p>
+              </div>
+              <div class="button-wrap mt-3"><a class="btn-text" href="#">Learn more</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+</body>
+
+</html>
+<?php
+include("components/footer.html");
+?>
