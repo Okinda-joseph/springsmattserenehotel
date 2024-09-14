672a2622b9adf178da966e7c1030d13f291455f0
DevNexus Tec. <160817446+Okinda-joseph@users.noreply.github.com>
2024-09-14 13:34:35 +0300

    Create spa.php

---
+++ spa.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- The required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Massage treatment</title>
+  <link rel="stylesheet" href="./css/boostrap.min.css">
+  <link rel="stylesheet" href="./css/boxicons.min.css">
+  <link rel="stylesheet" href="./css/owl.carousel.min.css">
+  <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="./css/styles.css">
+  <script src="https://kit.fontawesome.com/5bebd170da.js" crossorigin="anonymous"></script>
+  <!-- favicon -->
+  <link rel="shortcut icon" href="./assets/icons/logo.jpeg" type="image/x-icon">
+</head>
+
+<body>
+  <?php
+  include("components/header.html");
+  ?>
+  <!-- GENERAL BANNER -->
+  <div class="main-banner-wrapper">
+    <div class="main-banner d-flex align-items-center bg-cover" id="massage-banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="title-wrap">
+              <h1 class="title display-3 text-capitalize text-white">Spa Massage</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- MASSAGE BODY -->
+  <section id="main-content">
+    <div class="content-box-wrapper">
+      <div class="container">
+        <div class="main-title-wrap mb-4">
+          <h4 class="title">Experience spa massage treatment</h4>
+        </div>
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="row">
+              <div class="col-12">
+                <div class="content-box">
+                  <div class="text-wrap main-text">
+                    <p>
+                      Spa massage therapy is a holistic approach to wellness that helps in manipulating the body's muscles and soft tissues to promote relaxation, relieve tension, and enhance overall well-being to travellers. It's offered in our hotel serene and tranquil environment, often at spas or wellness centers. The hotel trained massage therapists uses various techniques to address specific needs and preferences of the guests clients.
+                    </p>
+                    <p>&nbsp;</p>
+                    <p>
+                      The benefits of spa massage therapy extend beyond just physical relaxation. It also help reduce stress, alleviate muscle pain and stiffness, improve circulation, boost immune function, and promote better sleep to travellers. Additionally, the massage therapy provides mental and emotional benefits, such as reducing anxiety and enhancing mood.
+                    </p>
+                    <p>&nbsp;</p>
+                    <p>
+                      The hotel offers a numerous types of massage techniques used in spa therapy, including Swedish massage, deep tissue massage, hot stone massage, aromatherapy massage, and Thai massage, among others. Each technique offers unique benefits and caters to different needs and preferences.
+                    </p>
+                    <p>&nbsp;</p>
+                    <p>
+                    <div class="main-img-box">
+                      <img loading="lazy" src="./assets/captions/spa (6).jpg" alt="spa-6">
+                    </div>
+                    </p>
+                    <p>&nbsp;</p>
+                    <p>
+                      springsMatt hotel, spa massage therapy offers a rejuvenating experience that can enhance both physical and mental well-being, making it a popular choice for our guest individuals seeking relaxation and stress relief in today's fast-paced world.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-8 col-sm-10">
+            <div class="title-wrap mb-4 title-left">
+              <h4 class="card-title title">New listings</h4>
+            </div>
+            <div class="side-box-wrapper">
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./heritage.php"><img loading="lazy" src="./assets/offers/caption ().jpg" alt="maasai-mara culture heritage" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Your best heritage & Serene Comfort</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Nestled in the heart of a historically rich landscape, Springsmatt Serene Hotel offers a unique blend of heritage charm and modern luxury.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./accommodation.php"><img loading="lazy" src="./assets/offers/caption (3).png" alt="Garden-suite-room" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Top 5 best charming rooms for you</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Our three 50 sq.m perennially popular Garden Suites promise an enchanting oasis in the very heart of the city. </p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./dining.php"><img loading="lazy" src="./assets/offers/caption (65).jpg" alt="meals" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Top 10 delicious menus for you</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Embark in a journey through the culinary traditions of the region, with our restaurant serving a delectable array of dishes inspired by historical recipes and local ingredients.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="box-wrap pt-3 pb-3">
+                <div class="box-img-wrap">
+                  <a href="./tour.php"><img loading="lazy" src="./assets/captions/tour (15).jpg" class="card-img-top box-img"></a>
+                </div>
+                <div class="box-body">
+                  <div class="title-wrap">
+                    <h6 class="title">Escape another world with prestige cityscape</h6>
+                  </div>
+                  <div class="box-text">
+                    <p>Experience the perfect blend of ambiance, flavor and hospitality.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="box-cat-wrapper">
+              <div class="title-wrap mt-4 title-left pb-4">
+                <h4 class="card-title title">special offers</h4>
+              </div>
+              <div class="box-list-wrap mt-3">
+                <ul class="cat-list">
+                  <li><a href="#">charming rooms experience</a></li>
+                  <li><a href="#">seafood meals at night</a></li>
+                  <li><a href="#">Tranquil Getaway Deal for winter</a></li>
+                  <li><a href="#">Serene Suite Sensation</a></li>
+                  <li><a href="#">prestige cityscape</a></li>
+                  <li><a href="#">Masai mara tour service</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
+<?php
+include("components/footer.html");
+?>
